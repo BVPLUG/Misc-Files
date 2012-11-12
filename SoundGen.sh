ffc504b6c4427cfafb06851d832a2658b6872f1a
Jai Luthra <me@jailuthra.in>
2012-11-13 01:30:38 +0530

    Decreased dependencies to festival only.
    Rather than two dependencies of text2wave and play
    the only dependency now is festival

--- SoundGen.sh
+++ SoundGen.sh
@@ -4,20 +4,12 @@ then
 echo " Usage : $ ./SoundGen <YourMessage>  " 
 exit -1
 fi
-which text2wave 2>/dev/null
+which festival 2>/dev/null
 if [ "$?" -eq 1 ]
 then 
-echo "'text2wave' not found,please try again after "
-exit -1
-fi
-which play 2>/dev/null 
-if [ "$?" -eq 1 ]
-then 
-echo "'play' required for running this script"
+echo "'festival' not found,please try again after "
 exit -1
 fi
 
-`echo "$@"|text2wave 1>a1.wav 2>/dev/null`
-play a1.wav &>/dev/null
-rm -f a1.wav
+echo "$@" | festival --tts
 exit 0
